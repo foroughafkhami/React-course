86763634f4b231c81540f93fc3af80d4f3f00f83
foroughafkhami <foroughfarnaz80@gmail.com>
2025-03-05 18:25:03 +0330

    complete 04-steps

--- 04-steps/04-Note.md
+++ 04-steps/04-Note.md
@@ -3,3 +3,26 @@
 > props: data comes from outside the component
 > state: component holds its own data and over time, change the UI as a result of an action
 > updating state triggers react to update the component(keeps the UI insync with the data)
+
+# hooks
+
+> all the React functions that start with use are hooks.like useState,useEffect,useReducer
+> we can use hooks only on the top level of the functions
+
+# Update state
+
+> When we set state based on the current state,we should use callback function
+
+```js
+setStep((s) => s + 2);
+```
+
+# UI
+
+> is the reflection of data changing over time
+
+# CHALLENGES
+
+## Number 1
+
+> https://codesandbox.io/p/sandbox/bold-pine-yykcqc?file=%2Fsrc%2FApp.js%3A70%2C25
--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -7,37 +7,56 @@ const messages = [
 ];
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  // const [test, setTest] = useState({ name: "Forough" });
   function handlePrevious() {
-    setStep(step - 1);
+    if (step > 1) {
+      setStep((s) => s - 1);
+    }
   }
   function handleNext() {
-    setStep(step + 1);
+    if (step < 3) {
+      setStep((s) => s + 2);
+
+      // BAD PRACTICE
+      // test.name="Fred"
+
+      // setTest({ name: "Fred" });
+    }
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
