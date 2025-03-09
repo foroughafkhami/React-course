85e8f018e300f86bece6703572b31feb7d7ef234
foroughafkhami <foroughfarnaz80@gmail.com>
2025-03-09 14:38:10 +0330

    add some prop to improve reusability

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { use, useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating="10" onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} starts</p>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      meassages={["Terrible", "Bad", "Ok", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={5}
+      color="red"
+      size={24}
+      className="test"
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
