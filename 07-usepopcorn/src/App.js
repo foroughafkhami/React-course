95dcb95f1bebebec20dd7a8dca8d84fb084dfde4
foroughafkhami <foroughfarnaz80@gmail.com>
2025-04-08 18:09:58 +0330

    ref to persist data between renders(in star rating)

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -262,6 +262,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
+
+  const countRef = useRef(0);
+
+  useEffect(
+    function () {
+      if (userRating) countRef.current++;
+    },
+    [userRating]
+  );
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   // const isWatched = watched.find((obj) => obj.imdbID === selectedId);
   const watchedUserRating = watched.find(
@@ -302,6 +311,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     };
     onAddWatched(newWatchedMovie);
     onCloseMovie();
