c514992f51e3654423eb39d96f9907be5cc4dd0b
foroughafkhami <foroughfarnaz80@gmail.com>
2025-04-12 17:54:40 +0330

    loading questions from a fake API is done

--- 10-react-quiz/Note.md
+++ 10-react-quiz/Note.md
@@ -4,3 +4,25 @@
 
 > reducer is a pure function that takes current state and action, and return the next state
 > ![alt text](manageUseReducer.png)
+
+# Create fake API
+
+1- npm i json-server
+2- create npm script in package.json file by add this line to the script:
+
+```js
+    "server": "json-server --watch data/questions.json --port 9000"
+
+```
+
+3- Then we fetch data using useEffect hook in the App like this:
+
+```js
+const [state, dispatch] = useReducer(reducer, initialState);
+useEffect(function () {
+  fetch("http://localhost:9000/questions")
+    .then((res) => res.json())
+    .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+    .catch((err) => dispatch({ type: "dataFailed" }));
+}, []);
+```
--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,9 +1,36 @@
-import DateCounter from "./DateCounter";
-
+import Headers from "./Header";
+import Main from "./Main";
+import { useEffect, useReducer } from "react";
+const initialState = {
+  questions: [],
+  // 'loading','error','ready','active','finished'
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      return new Error("Action Unknown");
+  }
+}
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Headers />
+      <Main>
+        <p>1/15</p>
+        <p>Questions? </p>
+      </Main>
     </div>
   );
 }
