621fd9cf1902b39e675f9fc3290d999a4b5e7738
foroughafkhami <foroughfarnaz80@gmail.com>
2025-04-12 18:34:44 +0330

    display current question by useReducer

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -10,6 +10,7 @@ const initialState = {
   questions: [],
   // 'loading','error','ready','active','finished'
   status: "loading",
+  index: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -24,7 +25,10 @@ function reducer(state, action) {
   }
 }
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   const numQuestions = questions.length;
   useEffect(function () {
     fetch("http://localhost:9000/questions")
@@ -41,7 +45,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
