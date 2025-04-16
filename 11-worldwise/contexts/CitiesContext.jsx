f81e9093baedf49596d242a0e630c73e3cef3e05
foroughafkhami <foroughfarnaz80@gmail.com>
2025-04-16 13:07:44 +0330

    deleting new city is done

--- 11-worldwise/contexts/CitiesContext.jsx
+++ 11-worldwise/contexts/CitiesContext.jsx
@@ -48,7 +48,21 @@ function CitiesProvider({ children }) {
       const data = await res.json();
       setCities((cities) => [...cities, data]);
     } catch {
-      alert("There was an error loading date..");
+      alert("There was an error creating city..");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch {
+      alert("There was an error deleting city..");
     } finally {
       setIsLoading(false);
     }
@@ -56,7 +70,14 @@ function CitiesProvider({ children }) {
 
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
