6e0680307cfa6a6c8cf47a8a712d37278764dbe7
foroughafkhami <foroughfarnaz80@gmail.com>
2025-04-16 15:18:30 +0330

    add fake auth protecting the route

--- 11-worldwise/src/pages/AppLayout.jsx
+++ 11-worldwise/src/pages/AppLayout.jsx
@@ -2,6 +2,7 @@ import Sidebar from "../components/Sidebar";
 import Map from "../components/Map";
 import User from "../components/User";
 import styles from "./AppLayout.module.css";
+import ProtectedRoute from "./ProtectedRoute";
 function AppLayout() {
   return (
     <div className={styles.app}>
