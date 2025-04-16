3af346de996276d9c17227473df33a4aa35b49fd
foroughafkhami <foroughfarnaz80@gmail.com>
2025-04-16 17:36:00 +0330

    refactor react quiz to context api

--- 10-react-quiz/src-no-context/components/DateCounter.js
+++ 10-react-quiz/src-no-context/components/DateCounter.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { act, useReducer } from "react";
 
 const initialState = { count: 0, step: 1 };
 
--- 10-react-quiz/src-no-context/components/Question.js
+++ 10-react-quiz/src-no-context/components/Question.js
@@ -1,8 +1,5 @@
-import { useQuiz } from "../contexts/QuizContext";
 import Options from "./Options";
-function Question() {
-  const { questions, dispatch, answer, index } = useQuiz();
-  const question = questions.at(index);
+function Question({ question, dispatch, answer }) {
   return (
     <div>
       <h4>{question.question}</h4>
